3338a64ecff8e73b26e8da76479b972f463f163c
Zhulhian <Daniel.kh555@gmail.com>
2016-03-08 10:37:26 +0100

    Added updated losescreen art, upated the Valley map, added floorboards. Have to find a better system for getting tiles.

--- src/seaofdecay/Tile.java
+++ src/seaofdecay/Tile.java
@@ -7,9 +7,12 @@ import seaofdecay.util.asciipanel.AsciiPanel;
 public enum Tile {
 	FUNGI_FLOOR((char)34, new Color(152, 193, 193), new Color(34, 72, 72)), //34
 	FUNGI_WALL((char)6, new Color(34, 102, 102), new Color(152, 193, 193)), // 158 is good as well.
+
 	VALLEY_WALL('#', new Color(255, 153, 51), new Color(255, 217, 102)),
-	VALLEY_GRASS((char)247, new Color(0, 217, 108), new Color(0, 140, 70)),
+	VALLEY_GRASS((char)247, new Color(0, 217, 108), new Color(0, 140, 105)),
 	VALLEY_PORTAL((char)234, AsciiPanel.brightBlue, AsciiPanel.brightCyan),
+	VALLEY_FLOORBOARD((char)240, new Color(102, 82, 51), new Color(128, 102, 64)),
+
 	BOUNDS('?', AsciiPanel.brightBlack, AsciiPanel.black);
 
 	public boolean isDiggable() {
--- src/seaofdecay/WorldBuilder.java
+++ src/seaofdecay/WorldBuilder.java
@@ -4,85 +4,88 @@ import seaofdecay.screens.Screen;
 import seaofdecay.util.xpreader.XPFile;
 
 public class WorldBuilder {
-    private int width;
-    private int height;
-    private Tile[][] tiles;
+	private int width;
+	private int height;
+	private Tile[][] tiles;
 
-    public WorldBuilder(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.tiles = new Tile[width][height];
-    }
+	public WorldBuilder(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.tiles = new Tile[width][height];
+	}
 
-    public World build() {
-        return new World(tiles);
-    }
+	public World build() {
+		return new World(tiles);
+	}
 
-    private WorldBuilder randomizeTiles() {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                tiles[x][y] = Math.random() < 0.5 ? Tile.FUNGI_FLOOR : Tile.FUNGI_WALL;
-            }
-        }
-        return this;
-    }
+	private WorldBuilder randomizeTiles() {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				tiles[x][y] = Math.random() < 0.5 ? Tile.FUNGI_FLOOR : Tile.FUNGI_WALL;
+			}
+		}
+		return this;
+	}
 
-    private WorldBuilder smooth(int times) {
+	private WorldBuilder smooth(int times) {
 
-        // So many nested for-loops... *cries a little bit*
+		// So many nested for-loops... *cries a little bit*
 
-        Tile[][] smoothedTiles = new Tile[width][height];
-        for (int time = 0; time < times; time++) {
+		Tile[][] smoothedTiles = new Tile[width][height];
+		for (int time = 0; time < times; time++) {
 
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-                    int floorTiles = 0;
-                    int wallTiles = 0;
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					int floorTiles = 0;
+					int wallTiles = 0;
 
-                    for (int ox = -1; ox < 2; ox++) {
-                        for (int oy = -1; oy < 2; oy++) {
+					for (int ox = -1; ox < 2; ox++) {
+						for (int oy = -1; oy < 2; oy++) {
 
-                            if (x + ox < 0 || x + ox >= width
-                                    || y + oy < 0 || y + oy >= height) {
-                                continue;
-                            }
+							if (x + ox < 0 || x + ox >= width
+									|| y + oy < 0 || y + oy >= height) {
+								continue;
+							}
 
-                            if (tiles[x + ox][y + oy] == Tile.FUNGI_FLOOR)
-                                floorTiles++;
-                            else
-                                wallTiles++;
-                        }
-                    }
-                    smoothedTiles[x][y] = floorTiles >= wallTiles ? Tile.FUNGI_FLOOR : Tile.FUNGI_WALL;
-                }
-            }
-            tiles = smoothedTiles;
-        }
-        return this;
-    }
+							if (tiles[x + ox][y + oy] == Tile.FUNGI_FLOOR)
+								floorTiles++;
+							else
+								wallTiles++;
+						}
+					}
+					smoothedTiles[x][y] = floorTiles >= wallTiles ? Tile.FUNGI_FLOOR : Tile.FUNGI_WALL;
+				}
+			}
+			tiles = smoothedTiles;
+		}
+		return this;
+	}
 
-    public WorldBuilder makeValley() {
-        XPFile valley = Screen.resMgr.getRes("SoD_Valley.xp");
-        for (int x = 0; x < valley.layer(0).width; x++) {
-            for (int y = 0; y < valley.layer(0).height; y++) {
-                switch (valley.layer(0).data[x][y].code) {
-                    case '#':
-                        tiles[x][y] = Tile.VALLEY_WALL;
-                        break;
-                    case (char)247:
-                        tiles[x][y] = Tile.VALLEY_GRASS;
-                        break;
-                    case (char)234:
-                        tiles[x][y] = Tile.VALLEY_PORTAL;
-                        break;
-                    default: tiles[x][y] = Tile.BOUNDS; break;
-                }
-            }
-        }
-        return this;
-    }
+	public WorldBuilder makeValley() {
+		XPFile valley = Screen.resMgr.getRes("SoD_Valley.xp");
+		for (int x = 0; x < valley.layer(0).width; x++) {
+			for (int y = 0; y < valley.layer(0).height; y++) {
+				switch (valley.layer(0).data[x][y].code) {
+					case '#':
+						tiles[x][y] = Tile.VALLEY_WALL;
+						break;
+					case (char)247:
+						tiles[x][y] = Tile.VALLEY_GRASS;
+						break;
+					case (char)234:
+						tiles[x][y] = Tile.VALLEY_PORTAL;
+						break;
+					case (char)240:
+						tiles[x][y] = Tile.VALLEY_FLOORBOARD;
+						break;
+					default: tiles[x][y] = Tile.BOUNDS; break;
+				}
+			}
+		}
+		return this;
+	}
 
-    public WorldBuilder makeCaves() {
-        return randomizeTiles().smooth(8);
-    }
+	public WorldBuilder makeCaves() {
+		return randomizeTiles().smooth(8);
+	}
 }
